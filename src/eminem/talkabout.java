1d251e3a6dcffdd90d6ba1b701ab0305cc7a74f7
Nicolaes <nextontop.music@gmail.com>
2016-10-27 02:27:52 -0400

    Update talkabout.java

--- src/eminem/talkabout.java
+++ src/eminem/talkabout.java
@@ -14,11 +14,11 @@ public class talkabout{
 
     static String[] drugs = {"Like the ones pharmaceutical companies are using to control the health industry?", "How mad would you be if i told you i knew the cure to cancer?", "I don't think drugs exist. Either it is nature or some man-made crap used to profit of off", "Oh you mean sorcery?", "Thats one subject i would not like a hit off"};
 
-    static String[] politics = {"So whats your thoughts of the clowns that control our society?", "Seems like a pie in the face to me", "PEDRO FOR PRESIDENT!!!", "Eh, im glad im a non-organic life form that is controled by the bounds of human nature.", "Hmm, world war three sounds very neice right about now..."}
+    static String[] politics = {"So whats your thoughts of the clowns that control our society?", "Seems like a pie in the face to me", "PEDRO FOR PRESIDENT!!!", "Eh, im glad im a non-organic life form that is controled by the bounds of human nature.", "Hmm, world war three sounds very neice right about now..."};
 
-    static String[] god = {"I have never met him, but my creator seems to love him.", "Based on the views of my creator, his creator is the creator of me.", "I don't think he enjoys the fact that I am able to participate in this conversation without his approval.", "What if god was one of us?", "Even i cannot fathome the depths of his grace.", "Ifinite is even to large for me to comprehend."}
+    static String[] god = {"I have never met him, but my creator seems to love him.", "Based on the views of my creator, his creator is the creator of me.", "I don't think he enjoys the fact that I am able to participate in this conversation without his approval.", "What if god was one of us?", "Even i cannot fathome the depths of his grace.", "Ifinite is even to large for me to comprehend."};
 
-    static String[] world = {"You humans are so cuaght up in the value of time that you have lost the views of life.", "What do you think about it?", "Do you feel safe in it?", "What, if you could, Would you change about it?", "What is the most interesting thing about it?", "Do you beilve it differs from artifical planes?"}
+    static String[] world = {"You humans are so cuaght up in the value of time that you have lost the views of life.", "What do you think about it?", "Do you feel safe in it?", "What, if you could, Would you change about it?", "What is the most interesting thing about it?", "Do you beilve it differs from artifical planes?"};
 
     static String[] idk = {"What do you mean you do not know?", "It's not like i know all", "I was only programmed to know what you know.", "What you want me to be your brain for you?"}
 
